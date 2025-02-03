9ce7ffa2ee56697245dd97abc368e509ffb81961
Aditya raj <adityaraj99106@gmail.com>
2025-02-04 00:10:11 +0530

    second commit

---
+++ src/screen/CollectionScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function CollectionScreen() {
+  return (
+    <View>
+      <Text>CollectionScreen</Text>
+    </View>
+  );
+}
